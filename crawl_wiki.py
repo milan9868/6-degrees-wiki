8362810b941da5305d458f9b316574e82a87de76
Milan Stojanovic <milanstojanovic998@gmail.com>
2024-08-22 20:06:13 +0200

    Back baby

--- crawl_wiki.py
+++ crawl_wiki.py
@@ -4,13 +4,26 @@ from bs4 import BeautifulSoup
 import re
 
 
-try:
-    html = urlopen("https://en.wikipedia.org/wiki/Beautiful_Soup_(HTML_parser)#:~:text=Beautiful%20Soup%20is%20a%20Python,is%20useful%20for%20web%20scraping.")
-except HTTPError:
-    print("error")
-else:    
-    bsObj = BeautifulSoup(html)
-    for link in bsObj.find("div", {"id":"bodyContent"}).findAll("a",
-    href = re.compile("^(/wiki/)((?!:).)*$")):
-        if 'href' in link.attrs:
-            print(link.attrs['href'])
+visited_pages = set()
+
+
+def get_links(url, depth = 0):
+    if depth > 2:
+        return
+    global visited_pages
+    try:
+        html = urlopen("https://sr.wikipedia.org"+url)
+    except HTTPError:
+        print("error")
+    else:    
+        bsObj = BeautifulSoup(html, features = 'html.parser')
+        for link in bsObj.find("div", {"id":"bodyContent"}).findAll("a",
+                                href = re.compile("^(/wiki/)((?!:).)*$")):
+            if 'href' in link.attrs:
+                if link.attrs['href'] not in visited_pages:
+                    visited_pages.add(link.attrs['href'])
+                    print(link.attrs['href'])
+                    get_links(link.attrs['href'], depth + 1)
+
+
+get_links("/wiki/Андрус_Ансип")
